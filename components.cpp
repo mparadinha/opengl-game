3a8183fb74dbabd7093d222f603d7c9814743710
Miguel Paradinha <miguel.p.paradinha@gmail.com>
2017-10-26 01:58:56 +0100

    more work on components. created document for writting design decision.

--- components.cpp
+++ components.cpp
@@ -3,10 +3,9 @@
 
 #include "components.h"
 
-struct PosRotScale {
-    glm::vec3 position;
-    glm::vec4 rotation;
-    glm::vec3 scale;
+struct Transformation : Component {
+    const static name = "transformation";
+    Transformation() { position = glm::vec3(0); rotation = glm::vec4(0); scale(1); }
 
     glm::mat4 model() {
         glm::mat4 m;
@@ -16,4 +15,28 @@ struct PosRotScale {
         m = glm::scale(m, scale);
         return m;
     }
+
+    glm::vec3 position;
+    glm::vec4 rotation;
+    glm::vec3 scale;
+
+};
+
+struct Drawable : Component {
+    // deps: transformation
+    const static name = "drawable";
+    Drawable(std::string file) : model(Model(file));
+
+    Model model;
+};
+
+// first person camera
+struct CameraFP : Component {
+    // deps: transformation
+    const static name = "camera_fp";
+};
+// 3rd person camera
+struct CameraTP : Component {
+    // deps: transformation
+    const static name = "camera_tp";
 };
